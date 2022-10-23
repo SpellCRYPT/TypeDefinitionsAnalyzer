f6f966bed82f4b18f912a16d1fc7dc2933fee374
Satsuki Akiba <anosatsuk124@gmail.com>
2022-10-24 06:58:11 +0900

    fix test file

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
 import analyzeTypes from './src/analyzeTypes';
 
 const source = `
-type fn = (a: number, b: number) => number;
-const fn2 = (a: number, b: number) => number;
-function fn3(a: number, b: number, c: number): string;
-function fn4(a: number, b: number, c: number): number;
+function fn1(a: number, b: number, c: number): string;
+function fn2(a: number, b: number, c: number): number;
 `;
 
 analyzeTypes(source);
